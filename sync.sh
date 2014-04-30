d0ad8981bb2137e9b029e7fd1ef4659c6554ead7
Tathagata Das <tathagata.das1565@gmail.com>
2014-04-30 15:19:20 -0700

    Removed About Us from footer

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 export DATE=`date +%Y-%m-%d-%H-%M` && ssh rohans@ischool.berkeley.edu -C "cd ~/public_html ; tar czvf cannycare-$DATE.tgz cannycare"
-scp *.html rohans@ischool.berkeley.edu:/home/rohans/public_html/cannycare/
+scp -r * rohans@ischool.berkeley.edu:/home/rohans/public_html/cannycare/
